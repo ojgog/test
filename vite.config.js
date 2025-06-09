7ea58583125dc298a3dd8fd3472ba9acf08b889f
ojgog <georgexilay@gmail.com>
2025-06-10 00:50:10 +0300

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/test/', // Repository name
+  base: '/test/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
